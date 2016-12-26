lambdajack/mkgmap-mkgmap

commit 32bfe42cda063b3b2801ac420191eaa42b25d951

Implement a new map output format "gmapi"
When the --gmapi option is given then the map will be created
in a directory named after the family name with an extension .gmap
This format is used to install maps on a Mac.

As with the --gmapsupp option, you can start with a set of .img files that
where created in a previous mkgmap run if you wish, as well as in one
step from the source OSM .pbf, .osm.gz etc files.



git-svn-id: https://svn.mkgmap.org.uk/mkgmap/mkgmap@3731 25d90789-57f7-4ee0-8453-03a3dfeeeb22

trunk/src/uk/me/parabola/mkgmap/main/Main.java
@@ -21,7 +21,6 @@ import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.Date;
 import java.util.HashMap;
@@ -30,6 +29,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
@@ -49,6 +49,7 @@ import uk.me.parabola.mkgmap.CommandArgsReader;
 import uk.me.parabola.mkgmap.Version;
 import uk.me.parabola.mkgmap.combiners.Combiner;
 import uk.me.parabola.mkgmap.combiners.FileInfo;
+import uk.me.parabola.mkgmap.combiners.GmapiBuilder;
 import uk.me.parabola.mkgmap.combiners.GmapsuppBuilder;
 import uk.me.parabola.mkgmap.combiners.MdrBuilder;
 import uk.me.parabola.mkgmap.combiners.MdxBuilder;
@@ -95,14 +96,16 @@ public class Main implements ArgumentProcessor {
 
 	private volatile int programRC = 0;
 
+	private final Map<String, Combiner> combinerMap = new HashMap<>();
+
 	/**
 	 * Used for unit tests
 	 */
-	public static void mainNoSystemExit(String[] args) {
+	public static void mainNoSystemExit(String... args) {
 		Main.mainStart(args);
 	}
 	
-	public static void main(String[] args) {
+	public static void main(String... args) {
 		int rc = Main.mainStart(args);
 		if (rc != 0)
 			System.exit(1);
@@ -115,7 +118,7 @@ public class Main implements ArgumentProcessor {
 	 *
 	 * @param args The command line arguments.
 	 */
-	private static int mainStart(String[] args) {
+	private static int mainStart(String... args) {
 		long start = System.currentTimeMillis();
 		System.out.println("Time started: " + new Date());
 		// We need at least one argument.
@@ -324,7 +327,7 @@ public class Main implements ArgumentProcessor {
 	}
 
 	public void removeOption(String opt) {
-		if ("tdbfile".equals(opt))
+		if (Objects.equals("tdbfile", opt))
 			createTdbFiles = false;
 	}
 
@@ -337,9 +340,9 @@ public class Main implements ArgumentProcessor {
 		if (!tdbBuilderAdded ){
 			OverviewMap overviewSource = new OverviewMapDataSource();
 			OverviewBuilder overviewBuilder = new OverviewBuilder(overviewSource);
-			addCombiner(overviewBuilder);
+			addCombiner("img", overviewBuilder);
 			TdbBuilder tdbBuilder = new TdbBuilder(overviewBuilder);
-			addCombiner(tdbBuilder);
+			addCombiner("tdb", tdbBuilder);
 			tdbBuilderAdded = true;
 		}
 	}
@@ -395,14 +398,14 @@ public class Main implements ArgumentProcessor {
 		}
 		int checked = 0;
 		for (String name : names) {
-			if (styleOption != null && !name.equals(styleOption))
+			if (!Objects.equals(name, styleOption))
 				continue;
 			if (names.length > 1){
 				System.out.println("checking style: " + name);
 			}
 			++checked;
 			boolean performChecks = true;
-			if ("classpath:styles".equals(styleFile) && !"default".equals(name)){
+			if (Objects.equals("classpath:styles", styleFile) && !Objects.equals("default", name)){
 					performChecks = false;
 			}
 			Style style = readOneStyle(name, performChecks);
@@ -446,7 +449,8 @@ public class Main implements ArgumentProcessor {
 		return "en";
 	}
 
-	private void addCombiner(Combiner combiner) {
+	private void addCombiner(String name, Combiner combiner) {
+		combinerMap.put(name, combiner);
 		combiners.add(combiner);
 	}
 
@@ -536,7 +540,7 @@ public class Main implements ArgumentProcessor {
 		for (Combiner c : combiners)
 			c.init(args);
 
-		Collections.sort(filenames, new Comparator<FilenameTask>() {
+		filenames.sort(new Comparator<FilenameTask>() {
 			public int compare(FilenameTask o1, FilenameTask o2) {
 				if (!o1.getFilename().endsWith(".img") || !o2.getFilename().endsWith(".img"))
 					return o1.getFilename().compareTo(o2.getFilename());
@@ -620,24 +624,30 @@ public class Main implements ArgumentProcessor {
 
 	private void fileOptions(CommandArgs args) {
 		boolean indexOpt = args.exists("index");
-		boolean gmapOpt = args.exists("gmapsupp");
+		boolean gmapsuppOpt = args.exists("gmapsupp");
 		boolean tdbOpt = args.exists("tdbfile");
+		boolean gmapiOpt = args.exists("gmapi");
+
 		if (tdbOpt || createTdbFiles){ 
 			addTdbBuilder();
 		}
 		if (args.exists("nsis")) {
-			addCombiner(new NsisBuilder());
+			addCombiner("nsis", new NsisBuilder());
 		}
-		if (gmapOpt) {
+		if (gmapsuppOpt) {
 			GmapsuppBuilder gmapBuilder = new GmapsuppBuilder();
 			gmapBuilder.setCreateIndex(indexOpt);
 
-			addCombiner(gmapBuilder);
+			addCombiner("gmapsupp", gmapBuilder);
 		}
 
-		if (indexOpt && (tdbOpt || !gmapOpt)) {
-			addCombiner(new MdrBuilder());
-			addCombiner(new MdxBuilder());
+		if (indexOpt && (tdbOpt || !gmapsuppOpt)) {
+			addCombiner("mdr", new MdrBuilder());
+			addCombiner("mdx", new MdxBuilder());
+		}
+
+		if (gmapiOpt) {
+			addCombiner("gmapi", new GmapiBuilder(combinerMap));
 		}
 	}
 

trunk/.idea/dictionaries/steve.xml
@@ -30,6 +30,7 @@
       <w>garmin's</w>
       <w>gawron</w>
       <w>geofabrik</w>
+      <w>gmapi</w>
       <w>gmapsupp</w>
       <w>gpsmapedit</w>
       <w>gtype</w>

trunk/Makefile
@@ -61,6 +61,7 @@ clean:
 	rm -f gmapsupp.img 632*.img
 	if [ -d 63240001 ]; then rmdir 6324000?; fi
 	rm -f 11112222.img 11112222.tdb
+	rm -rf *.gmapi *.gmap
 	rm -f osmmap*
 	rm -f ovm_*.img
 	rm -f g?.img

trunk/src/uk/me/parabola/mkgmap/combiners/Combiner.java
@@ -48,4 +48,7 @@ public interface Combiner {
 	 */
 	public void onFinish();
 
+	public default String getFilename() {
+		return null;
+	}
 }

trunk/src/uk/me/parabola/mkgmap/combiners/FileInfo.java
@@ -430,7 +430,7 @@ public class FileInfo {
 	}
 
 	public String getFamilyName() {
-		return args.get("family-name", "family name");
+		return args.get("family-name", "OSM map");
 	}
 
 	public String getSeriesName() {
@@ -491,5 +491,9 @@ public class FileInfo {
 
 	public boolean hasSortOrder() {
 		return sortOrderId != 0;
+	}
+
+	public String getOverviewName() {
+		return args.get("overview-mapname", "osmmap");
 	}
 }

trunk/src/uk/me/parabola/mkgmap/combiners/GmapiBuilder.java
@@ -1,0 +1,283 @@
+/*
+ * Copyright (C) 2016.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 or
+ * version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+package uk.me.parabola.mkgmap.combiners;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.Writer;
+import java.nio.ByteBuffer;
+import java.nio.channels.ByteChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
+
+import uk.me.parabola.imgfmt.ExitException;
+import uk.me.parabola.imgfmt.fs.DirectoryEntry;
+import uk.me.parabola.imgfmt.fs.FileSystem;
+import uk.me.parabola.imgfmt.fs.ImgChannel;
+import uk.me.parabola.imgfmt.sys.ImgFS;
+import uk.me.parabola.mkgmap.CommandArgs;
+
+import static java.nio.file.StandardOpenOption.*;
+
+/**
+ * Create a map in the gmapi format.
+ *
+ * This is directory tree containing an XML file describing the contents, and exploded versions of
+ * each .img file.
+ */
+public class GmapiBuilder implements Combiner {
+	private final static String NS = "http://www.garmin.com/xmlschemas/MapProduct/v1";
+
+	private final Map<String, Combiner> combinerMap;
+
+	private Path gmapDir;
+	private final Map<Integer, ProductInfo> productMap = new HashMap<>();
+
+	private String familyName;
+	private int familyId;
+	private short productVersion;
+
+	private String typFile;
+
+	public GmapiBuilder(Map<String, Combiner> combinerMap) {
+		this.combinerMap = combinerMap;
+	}
+
+	/**
+	 * Initialise with the command line arguments.  This is called after all
+	 * the command line arguments have been processed, but before any calls to
+	 * the {@link #onMapEnd} methods.
+	 *
+	 * @param args The command line arguments.
+	 */
+	public void init(CommandArgs args) {
+		familyName = args.get("family-name", "OSM map");
+		familyId = args.get("family-id", CommandArgs.DEFAULT_FAMILYID);
+		productVersion = (short) args.get("product-version", 100);
+
+		gmapDir = Paths.get(args.getOutputDir(), String.format("%s.gmap", familyName));
+	}
+
+	/**
+	 * This is called when an individual map is complete.
+	 *
+	 * @param info An interface to read the map.
+	 */
+	public void onMapEnd(FileInfo info) {
+
+		String fn = info.getFilename();
+		String mapname = info.getMapname();
+
+		int productId = info.getProductId();
+		if (!productMap.containsKey(productId))
+			productMap.put(productId, new ProductInfo(productId, info.getSeriesName(), info.getOverviewName()));
+
+		// Unzip the image into the product tile directory.
+		try {
+			if (info.isImg())
+				unzipImg(fn, mapname, productId);
+			else if (info.getKind() == FileKind.TYP_KIND)
+				typFile = info.getFilename();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * The complete map set has been processed.  Finish off anything that needs
+	 * doing.
+	 */
+	public void onFinish() {
+		try {
+			if (combinerMap.containsKey("mdx")) {
+				File file = new File(getFilenameFor("mdx"));
+				Files.copy(file.toPath(), gmapDir.resolve(file.getName()), StandardCopyOption.REPLACE_EXISTING);
+			}
+			if (combinerMap.containsKey("mdr")) {
+				File file = new File(getFilenameFor("mdr"));
+				unzipImg(file.getCanonicalPath(), gmapDir.resolve(nameWithoutExtension(file)));
+			}
+
+			if (typFile != null) {
+				File file = new File(typFile);
+				Files.copy(file.toPath(), gmapDir.resolve(file.getName()), StandardCopyOption.REPLACE_EXISTING);
+			}
+
+			for (ProductInfo info : productMap.values()) {
+				finishTdbFile(info);
+				unzipImg(getFilenameFor("img"), info.overviewName, info.id);
+			}
+
+			writeXmlFile(gmapDir);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private String nameWithoutExtension(File file) {
+		String name = file.getName();
+		int len = name.length();
+		if (len < 4)
+			return name;
+		return name.substring(0, len-4);
+	}
+
+	private void finishTdbFile(ProductInfo info) throws IOException {
+		Path tdbPath = Paths.get(getFilenameFor("tdb"));
+
+		Files.copy(tdbPath, gmapDir
+				.resolve(String.format("Product%d", info.id))
+				.resolve(String.format("%s.tdb", info.overviewName)), StandardCopyOption.REPLACE_EXISTING);
+	}
+
+	private void unzipImg(String srcImgName, String mapname, int productId) throws IOException {
+		Path destDir = Paths.get(gmapDir.toString(), "Product" + productId, mapname);
+
+		unzipImg(srcImgName, destDir);
+	}
+
+	private void unzipImg(String srcImgName, Path destDir) throws IOException {
+		FileSystem fs = ImgFS.openFs(srcImgName);
+		for (DirectoryEntry ent : fs.list()) {
+			String fullname = ent.getFullName();
+
+			try (ImgChannel f = fs.open(fullname, "r")) {
+				String name = displayName(fullname);
+				if (Objects.equals(name, "."))
+					continue;
+
+				Files.createDirectories(destDir);
+				copyToFile(f, destDir.resolve(name));
+			}
+		}
+	}
+
+	private void copyToFile(ImgChannel f, Path dest) {
+		ByteBuffer buf = ByteBuffer.allocate(8 * 1024);
+		try (ByteChannel outchan = Files.newByteChannel(dest, CREATE, WRITE, TRUNCATE_EXISTING)) {
+			while (f.read(buf) > 0) {
+				buf.flip();
+				outchan.write(buf);
+				buf.compact();
+			}
+		} catch (IOException e) {
+			throw new ExitException("Cannot write file " + e);
+		}
+	}
+
+	private String getFilenameFor(String kind) {
+		return combinerMap.get(kind).getFilename();
+	}
+
+	private String displayName(String fullname) {
+		return fullname.trim().replace("\000", "");
+	}
+
+	/**
+	 * An xml file contains similar information that is contained in the windows registry.
+	 *
+	 * @param gDir The directory where the Info.xml file will be created.
+	 */
+	private void writeXmlFile(Path gDir) {
+		Path infoFile = gDir.resolve("Info.xml");
+
+		XMLOutputFactory factory = XMLOutputFactory.newFactory();
+
+		try (Writer stream = Files.newBufferedWriter(infoFile)) {
+
+			XMLStreamWriter writer = factory.createXMLStreamWriter(stream);
+
+			writer.writeStartDocument("UTF-8", "1.0");
+			writer.setDefaultNamespace(NS);
+			writer.writeCharacters("\n");
+
+			writer.writeStartElement(NS,"MapProduct");
+			writer.writeDefaultNamespace(NS);
+			writer.writeCharacters("\n");
+
+			xmlElement(writer, "Name", familyName);
+			xmlElement(writer, "DataVersion", String.valueOf(productVersion));
+			xmlElement(writer, "DataFormat", "Original");
+			xmlElement(writer, "ID", String.valueOf(familyId));
+
+			if (combinerMap.containsKey("mdx")) {
+				String mdxFile = getFilenameFor("mdx");
+
+				File file = new File(mdxFile);
+				xmlElement(writer, "IDX", file.getName());
+			}
+
+			if (combinerMap.containsKey("mdr")) {
+				String mdrName = getFilenameFor("mdr");
+				File file = new File(mdrName);
+				xmlElement(writer, "MDR", nameWithoutExtension(file));
+			}
+
+			if (typFile != null) {
+				File file = new File(typFile);
+				xmlElement(writer, "TYP", file.getName());
+			}
+
+			for (ProductInfo prod : productMap.values()) {
+				writer.writeStartElement(NS, "SubProduct");
+				writer.writeCharacters("\n");
+
+				xmlElement(writer, "Name", prod.seriesName);
+				xmlElement(writer, "ID", String.valueOf(prod.id));
+				xmlElement(writer, "BaseMap", prod.overviewName);
+				xmlElement(writer, "TDB", String.format("%s.tdb", prod.overviewName));
+				xmlElement(writer, "Directory", String.format("Product%s", prod.id));
+				writer.writeEndElement();
+				writer.writeCharacters("\n");
+			}
+
+			writer.writeEndElement();
+			writer.writeEndDocument();
+			writer.flush();
+		} catch (XMLStreamException | IOException e) {
+			throw new ExitException("Could not create file " + infoFile + "; " + e);
+		}
+	}
+
+	private void xmlElement(XMLStreamWriter writer, String name, String value) throws XMLStreamException {
+		writer.writeCharacters(" ");
+		writer.writeStartElement(NS, name);
+		writer.writeCharacters(value);
+		writer.writeEndElement();
+		writer.writeCharacters("\n");
+	}
+
+	private static class ProductInfo {
+
+		private final String seriesName;
+		private final String overviewName;
+		private final int id;
+
+		public ProductInfo(int id, String seriesName, String overviewName) {
+			this.id = id;
+			this.seriesName = seriesName;
+			this.overviewName = overviewName;
+		}
+	}
+}

commit 517facb4540d7cb4edda3b502f3c94e6003e93f9

fix checkStyles option (regression from r3731)
git-svn-id: https://svn.mkgmap.org.uk/mkgmap/mkgmap@3741 25d90789-57f7-4ee0-8453-03a3dfeeeb22

trunk/src/uk/me/parabola/mkgmap/main/Main.java
@@ -398,7 +398,7 @@ public class Main implements ArgumentProcessor {
 		}
 		int checked = 0;
 		for (String name : names) {
-			if (!Objects.equals(name, styleOption))
+			if (styleOption != null && !Objects.equals(name, styleOption))
 				continue;
 			if (names.length > 1){
 				System.out.println("checking style: " + name);