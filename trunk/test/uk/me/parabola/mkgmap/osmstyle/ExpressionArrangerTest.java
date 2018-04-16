lambdajack/mkgmap-mkgmap

commit c2aac9c053b025c966f0f6e98b2ba5c14a5ef504

expr-func2.patch by Steve Ratcliffe
corrects handling of expresions like c!=d & a=$b




git-svn-id: https://svn.mkgmap.org.uk/mkgmap/mkgmap@4158 25d90789-57f7-4ee0-8453-03a3dfeeeb22

trunk/src/uk/me/parabola/mkgmap/osmstyle/ExpressionArranger.java
@@ -399,13 +399,16 @@ public class ExpressionArranger {
 			return 501;
 
 		// Everything else is 200+, put regex behind as they are probably a bit slower.
-		case REGEX:
-			return 201;
-		default:
-			// Non-indexable functions must go to the back.
-			if (!isIndexable(op))
-				return 1000;
+		case GT:
+		case GTE:
+		case LT:
+		case LTE:
 			return 200;
+		case REGEX:
+			return 210;
+
+		default:
+			return 1000;
 		}
 	}
 
@@ -429,6 +432,8 @@ public class ExpressionArranger {
 			if (first.isType(EQUALS)) {
 				keystring = first.getFirst().getKeyValue() + "=" + first.getSecond().getKeyValue();
 			} else if (first.isType(EXISTS)) {
+				if (!isIndexable(first))
+					throw new SyntaxException(scanner, "Expression cannot be indexed");
 				keystring = first.getFirst().getKeyValue() + "=*";
 			} else if (first.isType(NOT_EXISTS)) {
 				throw new SyntaxException(scanner, "Cannot start rule with tag!=*");
@@ -485,13 +490,12 @@ public class ExpressionArranger {
 	 *
 	 * This is done if the first term is not by itself indexable, but could be made so by pre-pending an EXISTS clause.
 	 */
-	private Op prepareWithExists(Op op) {
+	private static Op prepareWithExists(Op op) {
 		Op first = op;
 		if (first.isType(AND))
 			first = first.getFirst();
 
-		if (NEED_EXISTS.contains(first.getType()) && isIndexable(first)
-				|| first.isType(EQUALS) && first.getSecond().isType(FUNCTION))
+		if (NEED_EXISTS.contains(first.getType()) || first.isType(EQUALS) && first.getSecond().isType(FUNCTION))
 			return combineWithExists((BinaryOp) op);
 		else
 			return op;
@@ -502,7 +506,7 @@ public class ExpressionArranger {
 	 *
 	 * This is done if the first term is not by itself indexable, but could be made so by pre-pending an EXISTS clause.
 	 */
-	private AndOp combineWithExists(BinaryOp op) {
+	private static AndOp combineWithExists(BinaryOp op) {
 		Op first = op;
 		if (first.isType(AND))
 			first = first.getFirst();
@@ -515,26 +519,28 @@ public class ExpressionArranger {
 
 	/**
 	 * True if this expression is 'solved'.  This means that the first term is indexable or it is indexable itself.
+	 *
+	 * This is only used in the tests.
 	 */
 	public static boolean isSolved(Op op) {
 		switch (op.getType()) {
 		case NOT:
 			return false;
 		case AND:
-			return isIndexable(op.getFirst());
+			return isAndIndexable(prepareWithExists(op.getFirst()));
 		case OR:
 			Op or = op;
 			boolean valid = true;
 			do {
-				if (!isAndIndexable(or.getFirst()))
+				if (!isAndIndexable(prepareWithExists(or.getFirst())))
 					valid = false;
 				or = or.getSecond();
 			} while (or.isType(OR));
-			if (!isAndIndexable(or))
+			if (!isAndIndexable(prepareWithExists(or)))
 				valid = false;
 			return valid;
 		default:
-			return isIndexable(op);
+			return isIndexable(prepareWithExists(op));
 		}
 	}
 
@@ -553,11 +559,7 @@ public class ExpressionArranger {
 	 * True if this operation can be indexed.  It is a plain equality or Exists operation.
 	 */
 	private static boolean isIndexable(Op op) {
-		return op.isType(EQUALS)
-				&& ((ValueOp) op.getFirst()).isIndexable() && op.getSecond().isType(VALUE)
-				|| NEED_EXISTS.contains(op.getType()) && ((ValueOp) op.getFirst()).isIndexable()
-					&& (op.getSecond().isType(VALUE) || op.getSecond().isType(FUNCTION))
-				|| op.isType(EXISTS) && ((ValueOp) op.getFirst()).isIndexable();
+		return (op.isType(EQUALS) || op.isType(EXISTS)) && ((ValueOp) op.getFirst()).isIndexable();
 	}
 
 	/**

trunk/test/uk/me/parabola/mkgmap/osmstyle/ExpressionArrangerTest.java
@@ -28,6 +28,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 import static uk.me.parabola.mkgmap.osmstyle.ExpressionArranger.fmtExpr;
 import static uk.me.parabola.mkgmap.osmstyle.ExpressionArranger.isSolved;
+import static uk.me.parabola.mkgmap.osmstyle.eval.NodeType.AND;
 import static uk.me.parabola.mkgmap.osmstyle.eval.NodeType.EQUALS;
 
 
@@ -193,7 +194,18 @@ public class ExpressionArrangerTest {
 		assertTrue(isSolved(op));
 		assertTrue(op.getFirst().getType() != NodeType.FUNCTION);
 	}
-	
+
+	@Test
+	public void testEqualTagValue() {
+		Op op = createOp("c!=d & a=$b");
+		op = arranger.arrange(op);
+		Iterator<Op> it = arranger.prepareForSave(op);
+		op = it.next();
+		assertEquals(AND, op.getType());
+		assertEquals("$a=* & $a=$b & $c!=d", op.toString());
+		System.out.println(op.toString());
+	}
+
 	private Op createOp(String s) {
 		TokenScanner scanner = new TokenScanner("test.file", new StringReader(s));
 		ExpressionReader er = new ExpressionReader(scanner, FeatureKind.POLYLINE);

commit 1add8dc5d9293c85a30acbb44b4a807dc3d29abb

Fix for expression bug introduced in r4158
r4158 was itself an expression fix, but introduced its
own problems.

There was also a bug that no one noticed involving
double negation expressions eg: "!!(a<3)" that
was introduced separately.


git-svn-id: https://svn.mkgmap.org.uk/mkgmap/mkgmap@4165 25d90789-57f7-4ee0-8453-03a3dfeeeb22

trunk/.idea/misc.xml
@@ -72,7 +72,7 @@
   <component name="ProjectResources">
     <default-html-doctype>http://www.w3.org/1999/xhtml</default-html-doctype>
   </component>
-  <component name="ProjectRootManager" version="2" languageLevel="JDK_1_8" default="false" project-jdk-name="9" project-jdk-type="JavaSDK">
+  <component name="ProjectRootManager" version="2" languageLevel="JDK_1_8" default="false" project-jdk-name="1.8" project-jdk-type="JavaSDK">
     <output url="file://$PROJECT_DIR$/build/classes" />
   </component>
   <component name="SvnBranchConfigurationManager">
@@ -115,7 +115,6 @@
       </map>
     </option>
     <option name="myVersion" value="124" />
-    <option name="mySupportsUserInfoFilter" value="true" />
   </component>
   <component name="WebServicesPlugin" addRequiredLibraries="true" />
 </project>

trunk/src/uk/me/parabola/mkgmap/osmstyle/ExpressionArranger.java
@@ -106,8 +106,7 @@ public class ExpressionArranger {
 			break;
 
 		default:
-			// Should not happen
-			assert false;
+			break;
 		}
 
 		return op;
@@ -375,13 +374,16 @@ public class ExpressionArranger {
 	 * common tag, it would be better to push it behind other tags.
 	 */
 	private int selectivity(Op op) {
+		// Operations that involve a non-indexable function must always go to the back.
+		if (op.getFirst().isType(FUNCTION)) {
+			if (!((ValueOp) op.getFirst()).isIndexable())
+				return 1000;
+		}
+
 		switch (op.getType()) {
 		case EQUALS:
-			// Non-indexable functions must go to the back.
-			if (!isIndexable(op))
-				return 1000;
-
 			return 0;
+
 		case EXISTS:
 			return 100;
 

trunk/.idea/runConfigurations/RulesTest.xml
@@ -1,0 +1,10 @@
+<component name="ProjectRunConfigurationManager">
+  <configuration default="false" name="RulesTest" type="Application" factoryName="Application">
+    <option name="MAIN_CLASS_NAME" value="main.RulesTest" />
+    <module name="mkgmap" />
+    <option name="PROGRAM_PARAMETERS" value="--max-rules 10000 --errors --stop-on-fail --arr" />
+    <option name="VM_PARAMETERS" value="-ea -Xmx1000m" />
+    <option name="WORKING_DIRECTORY" value="file://$PROJECT_DIR$" />
+    <method />
+  </configuration>
+</component>

trunk/test/main/RulesTest.java
@@ -30,7 +30,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.EnumSet;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Objects;
 import java.util.Random;
@@ -70,8 +69,6 @@ import uk.me.parabola.mkgmap.scan.SyntaxException;
 import uk.me.parabola.mkgmap.scan.TokenScanner;
 
 import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
-import static uk.me.parabola.mkgmap.osmstyle.ExpressionArranger.fmtExpr;
-import static uk.me.parabola.mkgmap.osmstyle.ExpressionArranger.isSolved;
 import static uk.me.parabola.mkgmap.osmstyle.eval.NodeType.*;
 
 public class RulesTest {

trunk/test/uk/me/parabola/mkgmap/osmstyle/ExpressionArrangerTest.java
@@ -196,6 +196,16 @@ public class ExpressionArrangerTest {
 	}
 
 	@Test
+	public void testExistsAndFunctionWithOr() {
+		Op op = createOp("area_size() < 25000 & (fixme = * | FIXME=*) [0x2]");
+		System.out.println(fmtExpr(op));
+		op = arranger.arrange(op);
+		System.out.println(fmtExpr(op));
+		assertTrue(isSolved(op));
+		assertTrue(op.getFirst().getType() != NodeType.FUNCTION);
+	}
+
+	@Test
 	public void testEqualTagValue() {
 		Op op = createOp("c!=d & a=$b");
 		op = arranger.arrange(op);