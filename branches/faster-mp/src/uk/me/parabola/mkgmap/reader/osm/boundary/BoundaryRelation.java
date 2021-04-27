lambdajack/mkgmap-mkgmap

commit c426f97d7dc16cafabb691631db4e55603c2e2d5

divide&conquer: very experimental, but much faster with really complex multipolygons: - divide multipolygon into two halves if any ring has > 2000 nodes, do this again with each half to produce smaller lists of closed ways (a partition) with lesser complexity - process each partition like a single multipolygon TODO: error reporting and calculation of special size fields, maybe use better structures
git-svn-id: https://svn.mkgmap.org.uk/mkgmap/mkgmap@4679 25d90789-57f7-4ee0-8453-03a3dfeeeb22

branches/faster-mp/src/uk/me/parabola/mkgmap/reader/osm/boundary/BoundaryRelation.java
@@ -82,7 +82,7 @@ public class BoundaryRelation extends MultiPolygonRelation {
 	
 			BitSet holeIndexes = checkRoleAgainstGeometry(currentPolygon, unfinishedPolygons, nestedOuterPolygons, nestedInnerPolygons);
 	
-			ArrayList<PolygonStatus> holes = getPolygonStatus(holeIndexes,
+			ArrayList<PolygonStatus> holes = getPolygonStatus(polygons, holeIndexes,
 					(currentPolygon.outer ? ROLE_INNER : ROLE_OUTER));
 
 			// these polygons must all be checked for holes

branches/faster-mp/src/uk/me/parabola/mkgmap/reader/osm/MultiPolygonCutter.java
@@ -43,15 +43,18 @@ public class MultiPolygonCutter {
 	private static final Logger log = Logger.getLogger(MultiPolygonCutter.class);
 	private final MultiPolygonRelation rel;
 	private final Area tileArea;
+	private final Long2ObjectOpenHashMap<Coord> commonCoordMap;
 
 	/**
 	 * Create cutter for a given MP-relation and tile
 	 * @param multiPolygonRelation the MP-relation
 	 * @param tileArea the java area of the tile
 	 */
-	public MultiPolygonCutter(MultiPolygonRelation multiPolygonRelation, Area tileArea) {
+	public MultiPolygonCutter(MultiPolygonRelation multiPolygonRelation, Area tileArea, Long2ObjectOpenHashMap<Coord> commonCoordMap) {
 		rel = multiPolygonRelation;
 		this.tileArea = tileArea;
+		this.commonCoordMap = commonCoordMap;
+
 	}
 
 	/**
@@ -101,33 +104,14 @@ public class MultiPolygonCutter {
 			finishedAreas.addAll(outerAreas);
 		} else if (outerAreas.size() == 1) {
 			// there is one outer area only
-			// it is checked before that all inner areas are inside this outer area
+			// it is assumed that all inner areas are inside this outer area
 			AreaCutData initialCutData = new AreaCutData();
 			initialCutData.outerArea = outerAreas.get(0);
 			initialCutData.innerAreas = innerAreas;
 			areasToCut.add(initialCutData);
 		} else {
-			// multiple outer areas
-			for (Area outerArea : outerAreas) {
-				AreaCutData initialCutData = new AreaCutData();
-				initialCutData.outerArea = outerArea;
-				initialCutData.innerAreas = new ArrayList<>(innerAreas
-						.size());
-				for (Area innerArea : innerAreas) {
-					if (outerArea.getBounds2D().intersects(
-						innerArea.getBounds2D())) {
-						initialCutData.innerAreas.add(innerArea);
-					}
-				}
-				
-				if (initialCutData.innerAreas.isEmpty()) {
-					// this is either an error
-					// or the outer area has been cut into pieces on the tile bounds
-					finishedAreas.add(outerArea);
-				} else {
-					areasToCut.add(initialCutData);
-				}
-			}
+			// initial data contains multiple outer areas
+			combineOuterAndInner(outerAreas, innerAreas, finishedAreas, areasToCut);
 		}
 
 		while (!areasToCut.isEmpty()) {
@@ -146,7 +130,7 @@ public class MultiPolygonCutter {
 				areaCutData.outerArea.subtract(cutPoint.getAreas().get(0));
 			else {
 				// first combine the areas that should be subtracted
-				Path2D.Double path = new Path2D.Double();
+				Path2D.Double path = new Path2D.Double(Path2D.WIND_NON_ZERO, innerAreas.size() * 20);
 				for (Area cutArea : cutPoint.getAreas()) {
 					path.append(cutArea, false);
 				}
@@ -198,7 +182,6 @@ public class MultiPolygonCutter {
 		
 		// convert the java.awt.geom.Area back to the mkgmap way
 		List<Way> cuttedOuterPolygon = new ArrayList<>(finishedAreas.size());
-		Long2ObjectOpenHashMap<Coord> commonCoordMap = new Long2ObjectOpenHashMap<>();
 		for (Area area : finishedAreas) {
 			Way w = singularAreaToWay(area, rel.getOriginalId());
 			if (w != null) {
@@ -231,7 +214,7 @@ public class MultiPolygonCutter {
 	/**
 	 * Intersect area with cut rectangle.
 	 * Find the intersection of the rectangle with the original polygon and decide if the result is added to the 
-	 * finished areas or if another cut is needed. 
+	 * finished areas or if another cut is needed.
 	 * 
 	 * @param cutRect the cut rectangle
 	 * @param areaCutData the cut data
@@ -248,12 +231,27 @@ public class MultiPolygonCutter {
 			finishedAreas.addAll(dividedAreas);
 			return;
 		} 
+		combineOuterAndInner(dividedAreas, areaCutData.innerAreas, finishedAreas, areasToCut);
+	}
 
-		for (Area nextOuterArea : dividedAreas) {
+	/**
+	 * Combine each outer area which all those inner area that are likely to
+	 * intersect the outer. If no inner is found the outer area is finished, else
+	 * the new combination is added to the queue.
+	 * 
+	 * @param outerAreas    list of outer areas
+	 * @param innerAreas    list of all inner areas
+	 * @param finishedAreas list of finished areas
+	 * @param areasToCut    queue with unfinished cut data
+	 */
+	private static void combineOuterAndInner(List<Area> outerAreas, List<Area> innerAreas,
+			Collection<Area> finishedAreas, Queue<AreaCutData> areasToCut) {
+		for (Area nextOuterArea : outerAreas) {
 			ArrayList<Area> nextInnerAreas = null;
 			// go through all remaining inner areas and check if they
 			// must be further processed with the nextOuterArea
-			for (Area nonProcessedInner : areaCutData.innerAreas) {
+			
+			for (Area nonProcessedInner : innerAreas) {
 				if (nextOuterArea.intersects(nonProcessedInner.getBounds2D())) {
 					if (nextInnerAreas == null) {
 						nextInnerAreas = new ArrayList<>();
@@ -291,19 +289,18 @@ public class MultiPolygonCutter {
 			} else {
 				return cutPoint2;
 			}
-			
 		}
 		
-		ArrayList<Area> innerStart = new ArrayList<>(areaData.innerAreas);
+		ArrayList<Area> innersSorted = new ArrayList<>(areaData.innerAreas);
 		
 		CutPoint bestCutPoint = null;
 		for (CoordinateAxis axis : CoordinateAxis.values()) {
 			CutPoint currentCutPoint = new CutPoint(axis, outerBounds);
 
-			innerStart.sort(axis == CoordinateAxis.LONGITUDE ? COMP_LONG_START: COMP_LAT_START);
+			innersSorted.sort(axis == CoordinateAxis.LONGITUDE ? COMP_LONG_START: COMP_LAT_START);
 
-			for (Area anInnerStart : innerStart) {
-				currentCutPoint.addArea(anInnerStart);
+			for (Area inner : innersSorted) {
+				currentCutPoint.addArea(inner);
 
 				if (bestCutPoint == null || currentCutPoint.compareTo(bestCutPoint) > 0) {
 					bestCutPoint = currentCutPoint.duplicate();

branches/faster-mp/src/uk/me/parabola/mkgmap/reader/osm/MultiPolygonRelation.java
@@ -38,12 +38,14 @@ import java.util.concurrent.LinkedBlockingQueue;
 import java.util.logging.Level;
 import java.util.stream.Collectors;
 
+import it.unimi.dsi.fastutil.longs.Long2ObjectOpenHashMap;
 import uk.me.parabola.imgfmt.ExitException;
 import uk.me.parabola.imgfmt.app.Coord;
 import uk.me.parabola.log.Logger;
 import uk.me.parabola.util.IsInUtil;
 import uk.me.parabola.util.Java2DConverter;
 import uk.me.parabola.util.MultiIdentityHashMap;
+import uk.me.parabola.util.ShapeSplitter;
 
 /**
  * Representation of an OSM Multipolygon Relation.<br/>
@@ -78,7 +80,7 @@ public class MultiPolygonRelation extends Relation {
 	
 	private Map<Long, Way> mpPolygons = new LinkedHashMap<>();
 
-	private List<JoinedWay> polygons;
+	protected List<JoinedWay> polygons;
 	
 	// Various BitSets which relate to the content of field polygons  
 	private List<BitSet> containsMatrix;
@@ -94,7 +96,7 @@ public class MultiPolygonRelation extends Relation {
 	
 	private double largestSize;
 	private JoinedWay largestOuterPolygon;
-	
+	private Long2ObjectOpenHashMap<Coord> commonCoordMap = new Long2ObjectOpenHashMap<>();
 	protected Set<Way> outerWaysForLineTagging;
 
 	private final uk.me.parabola.imgfmt.app.Area tileBounds;
@@ -631,7 +633,7 @@ public class MultiPolygonRelation extends Relation {
 		return outmostPolygons;
 	}
 
-	protected ArrayList<PolygonStatus> getPolygonStatus(BitSet outmostPolygons, String defaultRole) {
+	protected static ArrayList<PolygonStatus> getPolygonStatus(List<JoinedWay> polygons, BitSet outmostPolygons, String defaultRole) {
 		ArrayList<PolygonStatus> polygonStatusList = new ArrayList<>();
 		outmostPolygons.stream().forEach(polyIndex -> {
 			// polyIndex is the polygon that is not contained by any other
@@ -662,6 +664,7 @@ public class MultiPolygonRelation extends Relation {
 	 */
 	public final void processElements() {
 		log.info("Processing multipolygon", toBrowseURL());
+//		long t1 = System.currentTimeMillis();
 		
 		// check if it makes sense to process the mp 
 		if (!isUsable()) { 
@@ -698,7 +701,7 @@ public class MultiPolygonRelation extends Relation {
 			cleanup();
 			return;
 		}
-
+		
 		analyseRelationRoles();
 		
 		if (outerPolygons.isEmpty()) {
@@ -707,6 +710,58 @@ public class MultiPolygonRelation extends Relation {
 			cleanup();
 			return;
 		}
+
+		
+		List<List<JoinedWay>> partitions = new ArrayList<>();
+//		polygons.sort((o1,o2) -> Integer.compare(o2.getPoints().size(), o1.getPoints().size()));
+//		if (true) // TODO: maybe not always?
+			divideLargest(polygons, partitions, 0);
+//		else 
+//			partitions.add(polygons);
+//		log.diagnostic("have " + partitions.size() + " partitions");
+		for (int i = 0; i < partitions.size(); i++) {
+			processPartition(partitions.get(i));
+		}
+		tagOuterWays();
+		
+		postProcessing();
+		cleanup();
+//		long dt = System.currentTimeMillis() - t1;
+//		log.diagnostic("processing MP relation " + this + " took " + dt + " ms");
+		
+	}
+
+
+	/**
+	 * Calculate the bounds of given collection of joined ways
+	 * @param polygons list of polygons
+	 * @return the bounds
+	 */
+	private static uk.me.parabola.imgfmt.app.Area calcBounds(Collection<JoinedWay> polygons) {
+		int minLat = Integer.MAX_VALUE;
+		int minLon = Integer.MAX_VALUE;
+		int maxLat = Integer.MIN_VALUE;
+		int maxLon = Integer.MIN_VALUE;
+		for (JoinedWay jw : polygons) {
+			if (jw.minLat < minLat)
+				minLat = jw.minLat;
+			if (jw.minLon < minLon)
+				minLon = jw.minLon;
+			if (jw.maxLat > maxLat)
+				maxLat = jw.maxLat;
+			if (jw.maxLon > maxLon)
+				maxLon = jw.maxLon;
+		}
+		return new uk.me.parabola.imgfmt.app.Area(minLat, minLon, maxLat, maxLon);
+	}
+
+
+	void processPartition(List<JoinedWay> partition) {
+		polygons = partition;
+		
+		analyseRelationRoles();
+		
+		assert !outerPolygons.isEmpty(): "no outer way in partition"; 
 
 		// check which polygons lie inside which other polygon 
 		createContainsMatrix();
@@ -742,15 +797,11 @@ public class MultiPolygonRelation extends Relation {
 		} while (outmostInnerFound);
 
 		
-		polygonWorkingQueue.addAll(getPolygonStatus(outmostPolygons, ROLE_OUTER));
+		polygonWorkingQueue.addAll(getPolygonStatus(polygons, outmostPolygons, ROLE_OUTER));
 		processQueue(polygonWorkingQueue, nestedOuterPolygons, nestedInnerPolygons);
 
 		doReporting(outmostInnerPolygons, unfinishedPolygons, nestedOuterPolygons, nestedInnerPolygons);
-		
-		tagOuterWays();
-		
-		postProcessing();
-		cleanup();
+
 	}
 
 	protected boolean isUsable() {
@@ -895,7 +946,7 @@ public class MultiPolygonRelation extends Relation {
 
 			BitSet holeIndexes = checkRoleAgainstGeometry(currentPolygon, unfinishedPolygons, nestedOuterPolygons, nestedInnerPolygons);
  
-			ArrayList<PolygonStatus> holes = getPolygonStatus(holeIndexes,
+			ArrayList<PolygonStatus> holes = getPolygonStatus(polygons, holeIndexes,
 					(currentPolygon.outer ? ROLE_INNER : ROLE_OUTER));
 
 			// these polygons must all be checked for holes
@@ -936,7 +987,7 @@ public class MultiPolygonRelation extends Relation {
 						innerWays.add(polygonHoleStatus.polygon);
 					}
 
-					MultiPolygonCutter cutter = new MultiPolygonCutter(this, tileArea);
+					MultiPolygonCutter cutter = new MultiPolygonCutter(this, tileArea, commonCoordMap);
 					singularOuterPolygons = cutter.cutOutInnerPolygons(currentPolygon.polygon, innerWays);
 				}
 				
@@ -1196,6 +1247,7 @@ public class MultiPolygonRelation extends Relation {
 		taggedOuterPolygons = null;
 		
 		largestOuterPolygon = null;
+		commonCoordMap = null;
 	}
 
 	/**
@@ -1296,22 +1348,26 @@ public class MultiPolygonRelation extends Relation {
 	/**
 	 * Checks if polygon1 contains polygon2 without intersection. 
 	 * 
-	 * @param polygon1
+	 * @param expectedOuter
 	 *            a closed way
-	 * @param polygon2
+	 * @param expectedInner
 	 *            a 2nd closed way
 	 * @return true if polygon1 contains polygon2 without intersection.
 	 */
-	private boolean calcContains(JoinedWay polygon1, JoinedWay polygon2) {
-		if (!polygon1.hasIdenticalEndPoints()) {
+	private boolean calcContains(JoinedWay expectedOuter, JoinedWay expectedInner) {
+		if (!expectedOuter.hasIdenticalEndPoints()) {
 			return false;
 		}
 		// check if the bounds of polygon2 are completely inside/enclosed the bounds
 		// of polygon1
-		if (!polygon1.getBounds().contains(polygon2.getBounds())) {
+		if (!expectedOuter.getBounds().contains(expectedInner.getBounds())) {
 			return false;
 		}
-		int	x = IsInUtil.isLineInShape(polygon2.getPoints(), polygon1.getPoints(), polygon2.getArea());
+		//TODO remove hack
+//		if (getId() == 9488835 && expectedOuter.getPoints().size() > 400_000
+//				&& !expectedInner.getOriginalWays().stream().anyMatch(w -> w.getId() == 197301581L))
+//			return true;
+		int	x = IsInUtil.isLineInShape(expectedInner.getPoints(), expectedOuter.getPoints(), expectedInner.getArea());
 		return (x & IsInUtil.OUT) == 0;
 	}
 
@@ -1568,6 +1624,21 @@ public class MultiPolygonRelation extends Relation {
 			updateBounds(originalWay.getPoints());
 		}
 
+		public JoinedWay(JoinedWay other, List<Coord> points) {
+			super(other.getOriginalId(), points);
+			markAsGeneratedFrom(other);
+			originalWays = new ArrayList<>(other.getOriginalWays());
+			intRole = other.intRole;
+			closedArtificially = other.closedArtificially;
+			// we have to initialize the min/max values
+			Coord c0 = points.get(0);
+			minLat = maxLat = c0.getLatitude();
+			minLon = maxLon = c0.getLongitude();
+
+			updateBounds(points);
+			
+		}
+
 		private byte roleToInt(String role) {
 			if (role == null)
 				return INT_ROLE_NULL;
@@ -1816,5 +1887,65 @@ public class MultiPolygonRelation extends Relation {
 			return polygon + "_" + outer;
 		}
 	}
+	
+	private void divideLargest(List<JoinedWay> partition, List<List<JoinedWay>> partitions, int depth) {
+		if (partition.isEmpty())
+			return;
+		//TODO: find out in what case dividing will produce false results in calcContains
+		// probably complex polygons with crossing /self intersecting ways will be problematic 
+		if (depth >= 10 || partition.size() < 2) {
+			partitions.add(partition);
+			return;
+		}
+		JoinedWay mostComplex = null;
+		for (JoinedWay jw : partition) {
+			if (mostComplex == null || mostComplex.getPoints().size() < jw.getPoints().size())
+				mostComplex = jw;
+		}
+		if (mostComplex.getPoints().size() > 2000) {
+			uk.me.parabola.imgfmt.app.Area fullArea = calcBounds(partition);
+			uk.me.parabola.imgfmt.app.Area[] areas;
+			final int niceSplitShift = 11; 
+			if (fullArea.getHeight() > fullArea.getWidth())
+				areas = fullArea.split(1, 2, niceSplitShift);
+			else 
+				areas = fullArea.split(2, 1, niceSplitShift);
+			// code to calculate dividingLine taken from MapSplitter
+			if (areas != null && areas.length == 2) {
+				int dividingLine = 0;
+				boolean isLongitude = false;
+				boolean commonLine = true;
+				if (areas[0].getMaxLat() == areas[1].getMinLat()) {
+					dividingLine = areas[0].getMaxLat();
+				} else if (areas[0].getMaxLong() == areas[1].getMinLong()) {
+					dividingLine = areas[0].getMaxLong();
+					isLongitude = true;
+				} else {
+					commonLine = false;
+					log.error("Split into 2 expects shared edge between the areas");
+				}
+				if (commonLine) {
+					List<JoinedWay> dividedLess = new ArrayList<>();
+					List<JoinedWay> dividedMore = new ArrayList<>();
+					for (int i = 0; i < partition.size(); i++) {
+						JoinedWay jw = partition.get(i);
+						
+						List<List<Coord>> lessList = new ArrayList<>();
+						List<List<Coord>> moreList = new ArrayList<>();
+						ShapeSplitter.splitShape(jw.getPoints(), dividingLine << Coord.DELTA_SHIFT, isLongitude,
+								lessList, moreList, commonCoordMap);
+						
+						lessList.forEach(part -> dividedLess.add(new JoinedWay(jw, part)));
+						moreList.forEach(part -> dividedMore.add(new JoinedWay(jw, part)));
+					}
+					divideLargest(dividedLess, partitions, depth + 1);
+					divideLargest(dividedMore, partitions, depth + 1);
+					return;
+				}
+			}
+		} 
+		partitions.add(partition);
+	}
+
 
 }

branches/faster-mp/src/uk/me/parabola/util/GpxCreator.java
@@ -1,5 +1,6 @@
 package uk.me.parabola.util;
 
+import java.awt.Shape;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.PrintWriter;
@@ -74,11 +75,14 @@ public class GpxCreator {
 	}
 
 	/**
-	 * Create gpx file(s) for java area 
+	 * Create gpx file(s) for java Shape. 
 	 * @param baseDir the base directory name
-	 * @param area the area to convert
+	 * @param shape the shape to convert
 	 */
-	public static void createJavaAreaGpx(String baseDir, java.awt.geom.Area area) {
+	public static void createShapeGpx(String baseDir, Shape shape) {
+		// have to convert to area to make sure that clockwise/counterclockwise idea works for inner/outer
+		java.awt.geom.Area area = shape instanceof java.awt.geom.Area ? (java.awt.geom.Area) shape
+				: new java.awt.geom.Area(shape);
 		List<List<Coord>> shapes = Java2DConverter.areaToShapes(area);
 		for (int i = 0; i < shapes.size(); i++) {
 			List<Coord> points = shapes.get(i);
@@ -100,7 +104,7 @@ public class GpxCreator {
 	}
 
 	public static void createGpx(String name, List<Coord> points) {
-		for (int i = 0; i < 2; i++){
+		for (int i = 1; i < 2; i++){ //TODO: revert 
 			String fname = name + (i==0 ? "_mu":"_hp");
 			try {
 				File f = new File(fname);

commit e095d3746ca8b66053aaeb686ae3902407f46c80

fix bug in BoundaryRelation introduced with r4679 (divide&conquer) - initialize area only once
git-svn-id: https://svn.mkgmap.org.uk/mkgmap/mkgmap@4683 25d90789-57f7-4ee0-8453-03a3dfeeeb22

branches/faster-mp/src/uk/me/parabola/mkgmap/reader/osm/boundary/BoundaryRelation.java
@@ -69,7 +69,9 @@ public class BoundaryRelation extends MultiPolygonRelation {
 	@Override
 	protected void processQueue(Queue<PolygonStatus> polygonWorkingQueue, BitSet nestedOuterPolygons,
 			BitSet nestedInnerPolygons) {
-		outerResultArea = new java.awt.geom.Area();
+		if (outerResultArea == null) {
+			outerResultArea = new java.awt.geom.Area();
+		}
 		
 		while (!polygonWorkingQueue.isEmpty()) {
 	