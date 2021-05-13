lambdajack/mkgmap-mkgmap

commit f7a615d627bf3e3444a0a3ae3011c81dbe711dc9

code simplification (osmstyle), no functional change intended - move common code in toString() methods to calcValueBuildersString(), it is only used when debugging - remove commented or dead code and some TODO statements - add @override - various other small changes suggested by sonarlint
git-svn-id: https://svn.mkgmap.org.uk/mkgmap/mkgmap@4549 25d90789-57f7-4ee0-8453-03a3dfeeeb22

trunk/src/uk/me/parabola/mkgmap/osmstyle/actions/AddAccessAction.java
@@ -12,10 +12,9 @@
  */
 package uk.me.parabola.mkgmap.osmstyle.actions;
 
-import java.util.List;
+import static uk.me.parabola.imgfmt.app.net.AccessTagsAndBits.ACCESS_TAGS_COMPILED;
 
 import uk.me.parabola.mkgmap.reader.osm.Element;
-import static uk.me.parabola.imgfmt.app.net.AccessTagsAndBits.*;
 
 /**
  * Add one value to all mkgmap access tags, optionally changing them if they already exist.  
@@ -41,7 +40,7 @@ public class AddAccessAction extends ValueBuildedAction {
 
 	public boolean perform(Element el) {
 		// 1st build the value
-		Element tags = valueTags!=null? valueTags: el;
+		Element tags = valueTags != null ? valueTags : el;
 		String accessValue = null;
 		for (ValueBuilder value : getValueBuilder()) {
 			accessValue = value.build(tags, el);
@@ -67,11 +66,14 @@ public class AddAccessAction extends ValueBuildedAction {
 	 * @param value the value to be set
 	 */
 	private void setTag(Element el, Short tagKey, String value) {
-		String tv = el.getTag(tagKey);
-		if (tv != null && !modify)
-			return;
-
-		el.addTag(tagKey, value);
+		if (modify) {
+			el.addTag(tagKey, value);
+		} else {
+			String tv = el.getTag(tagKey);
+			if (tv == null) {
+				el.addTag(tagKey, value);
+			}
+		}
 	}
 
 	public void setValueTags(Element valueTags) {
@@ -79,15 +81,6 @@ public class AddAccessAction extends ValueBuildedAction {
 	}
 
 	public String toString() {
-		StringBuilder sb = new StringBuilder();
-		sb.append(modify ? "setaccess " : "addaccess ");
-		List<ValueBuilder> values = getValueBuilder();
-		for (int i = 0; i < values.size(); i++) {
-			sb.append(values.get(i));
-			if (i < values.size() - 1)
-				sb.append(" | ");
-		}
-		sb.append(';');
-		return sb.toString();
+		return  modify ? "setaccess " : "addaccess " + calcValueBuildersString() + ";";
 	}
 }

trunk/src/uk/me/parabola/mkgmap/osmstyle/actions/AddTagAction.java
@@ -16,8 +16,6 @@
  */
 package uk.me.parabola.mkgmap.osmstyle.actions;
 
-import java.util.List;
-
 import uk.me.parabola.mkgmap.reader.osm.Element;
 import uk.me.parabola.mkgmap.reader.osm.TagDict;
 
@@ -48,12 +46,10 @@ public class AddTagAction extends ValueBuildedAction {
 	}
 
 	public boolean perform(Element el) {
-		if (!modify){
-			String tv = el.getTag(tagKey);
-			if (tv != null)
-				return false;
-		}
-		Element tags = valueTags!=null? valueTags: el;
+		if (!modify && el.getTag(tagKey) != null)
+			return false;
+		
+		Element tags = valueTags != null ? valueTags : el;
 
 		for (ValueBuilder value : getValueBuilder()) {
 			String newval = value.build(tags, el);
@@ -71,17 +67,6 @@ public class AddTagAction extends ValueBuildedAction {
 	}
 
 	public String toString() {
-		StringBuilder sb = new StringBuilder();
-		sb.append(modify ? "set " : "add ");
-		sb.append(tag);
-		sb.append("=");
-		List<ValueBuilder> values = getValueBuilder();
-		for (int i = 0; i < values.size(); i++) {
-			sb.append(values.get(i));
-			if (i < values.size() - 1)
-				sb.append(" | ");
-		}
-		sb.append(';');
-		return sb.toString();
+		return modify ? "set " : "add " + tag + "=" + calcValueBuildersString() + ";";
 	}
 }

trunk/src/uk/me/parabola/mkgmap/osmstyle/actions/Action.java
@@ -34,5 +34,5 @@ public interface Action {
 	 * @param el
 	 * @return true if one or more tags of the element were changed.
 	 */
-	public boolean perform(Element el);
+	boolean perform(Element el);
 }

trunk/src/uk/me/parabola/mkgmap/osmstyle/actions/ActionReader.java
@@ -35,7 +35,7 @@ import static uk.me.parabola.imgfmt.app.net.AccessTagsAndBits.*;
  * @author Steve Ratcliffe
  */
 public class ActionReader {
-	Set<String> VALID_ACCESS = new HashSet<>(Arrays.asList("yes", "no", "true", "false", "1", "0"));
+	private static final Set<String> VALID_ACCESS = new HashSet<>(Arrays.asList("yes", "no", "true", "false", "1", "0"));
 
 	private final TokenScanner scanner;
 

trunk/src/uk/me/parabola/mkgmap/osmstyle/actions/AddLabelAction.java
@@ -12,6 +12,9 @@
  */
 package uk.me.parabola.mkgmap.osmstyle.actions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import uk.me.parabola.mkgmap.reader.osm.Element;
 
 /**
@@ -21,6 +24,7 @@ import uk.me.parabola.mkgmap.reader.osm.Element;
  * We have a list of possible substitutions.
  */
 public class AddLabelAction extends ValueBuildedAction {
+	private final List<String> labels = new ArrayList<>(4);
 	
 	/**
 	 * Search for the first matching pattern and set the first unset element label
@@ -32,40 +36,32 @@ public class AddLabelAction extends ValueBuildedAction {
 	 * @return 
 	 */
 	public boolean perform(Element el) {
-		for (int index = 1; index <=4; index++) {
+		labels.clear();
+		for (int index = 1; index <= 4; index++) {
+			String tag = "mkgmap:label:" + index;
+			String label = el.getTag(tag);
 			// find the first unset label and set it
-			if (el.getTag("mkgmap:label:"+index) == null) {
-				for (ValueBuilder vb : getValueBuilder()) {
-					String s = vb.build(el, el);
-					if (s != null) {
-						// now check if the new label is different to all other labels
-						for (int n = index-1; n>= 1; n--) {
-							if (s.equals(el.getTag("mkgmap:label:"+n))) {
-								// value is equal to a previous label
-								// do not use it
-								return false;
-							}
-						}
-						
-						// set the label
-						el.addTag("mkgmap:label:"+index, s);
-						return true;
-					}
-				}
-				return false;
+			if (label != null) {
+				labels.add(label);
+				continue;
 			}
+			for (ValueBuilder vb : getValueBuilder()) {
+				String s = vb.build(el, el);
+				if (s != null) {
+					// now check if the new label is different to all other labels
+					if (labels.contains(s))
+						return false;
+					// set the label
+					el.addTag(tag, s);
+					return true;
+				}
+			}
+			return false;
 		}
 		return false;
 	}
 
 	public String toString() {
-		StringBuilder sb = new  StringBuilder();
-		sb.append("addlabel ");
-		for (ValueBuilder vb : getValueBuilder()) {
-			sb.append(vb);
-			sb.append(" | ");
-		}
-		sb.setLength(sb.length() - 3); 
-		return sb.toString();
+		return "addlabel " + calcValueBuildersString();
 	}
 }

trunk/src/uk/me/parabola/mkgmap/osmstyle/actions/CountryISOFilter.java
@@ -23,9 +23,6 @@ import uk.me.parabola.mkgmap.reader.osm.Element;
  */
 public class CountryISOFilter extends ValueFilter {
 
-	public CountryISOFilter() {
-	}
-
 	protected String doFilter(String value, Element el) {
 		if (value == null)
 			return value;

trunk/src/uk/me/parabola/mkgmap/osmstyle/actions/DeleteAllTagsAction.java
@@ -12,9 +12,7 @@
  */
 package uk.me.parabola.mkgmap.osmstyle.actions;
 
-import uk.me.parabola.imgfmt.app.Coord;
 import uk.me.parabola.mkgmap.reader.osm.Element;
-import uk.me.parabola.mkgmap.reader.osm.Node;
 
 /**
  * Delete all tags from an element. This is useful to stop its processing.
@@ -23,16 +21,9 @@ import uk.me.parabola.mkgmap.reader.osm.Node;
  */
 public class DeleteAllTagsAction implements Action {
 
-	// as long as there is not deleteAll method copy the tags of element without tags
-	private final Element noTagElement;
 	
-	public DeleteAllTagsAction() {
-		this.noTagElement = new Node(0, new Coord(0,0));
-	}
-
 	public boolean perform(Element el) {
-		// remove all tags by copying the tags from a no tag element
-		el.copyTags(noTagElement);
+		el.removeAllTags();
 		return true;
 	}
 

trunk/src/uk/me/parabola/mkgmap/osmstyle/actions/HeightFilter.java
@@ -31,10 +31,9 @@ public class HeightFilter extends ConvertFilter {
 			throw new SyntaxException(String.format("height filter reqires ft (feet) as target unit: '%s'", s));
 	}
 
+	@Override
 	public String doFilter(String value, Element el) {
 		String s = super.doFilter(value, el);
-		if (s != null)
-			s = "\u001f" + s;
-		return s;
+		return s != null ? "\u001f" + s : null;
 	}
 }

commit d564b39dcf706aa330aeca70b49f58eccdbb956b

fix error introduced with r4549: logged action rules did not contain target e.g. instead of "[set mkgmap:country=${mkgmap:admin_level2};]" only "[set ]" was logged Probably only visible when logging or debugging, else this would have caused massive trouble.
git-svn-id: https://svn.mkgmap.org.uk/mkgmap/mkgmap@4712 25d90789-57f7-4ee0-8453-03a3dfeeeb22

trunk/src/uk/me/parabola/mkgmap/osmstyle/actions/AddAccessAction.java
@@ -81,6 +81,6 @@ public class AddAccessAction extends ValueBuildedAction {
 	}
 
 	public String toString() {
-		return  modify ? "setaccess " : "addaccess " + calcValueBuildersString() + ";";
+		return  (modify ? "setaccess " : "addaccess ") + calcValueBuildersString() + ";";
 	}
 }

trunk/src/uk/me/parabola/mkgmap/osmstyle/actions/AddTagAction.java
@@ -67,6 +67,6 @@ public class AddTagAction extends ValueBuildedAction {
 	}
 
 	public String toString() {
-		return modify ? "set " : "add " + tag + "=" + calcValueBuildersString() + ";";
+		return (modify ? "set " : "add ") + tag + "=" + calcValueBuildersString() + ";";
 	}
 }