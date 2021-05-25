lambdajack/mkgmap-mkgmap

commit 2c55b17b4ea0b0b65317263d521d532470f34385

experimental: massive change to improve complex sea areas - for ovm_* : merge shapes and remove small holes which collaps to a point before calling MapSplitter - let ShapeMergeFilter merge as much as possible when resolution is below 24 - improve preserveHorizontalAndVerticalLines to presever fewer unneeded points - simplify MultiPolygonCutter (taken from the faster-mp branch and further simplified) - fix error in IsInUtil (also taken from the faster-mp branch)
Effects: 
- much smaller tiles when they contains complex multipolygons (esp. sea)
- fewer white spots in sea at low resolution

TODO: 
- make sure that this works well with generators for bounds.zip and sea.zip 
- maybe improve performance by merging sea only once instead of multiple times for each overview level.




git-svn-id: https://svn.mkgmap.org.uk/mkgmap/mkgmap@4729 25d90789-57f7-4ee0-8453-03a3dfeeeb22

branches/low-res-opt/src/uk/me/parabola/mkgmap/build/MapBuilder.java
@@ -816,6 +816,9 @@ public class MapBuilder implements Configurable {
 			Zoom zoom = map.createZoom(linfo.getLevel(), linfo.getBits());
 
 			for (SourceSubdiv srcDivPair : srcList) {
+				if (mergeShapes && isOverviewComponent) {
+					mergeShapesFirst(srcDivPair, zoom);
+				}
 
 				MapSplitter splitter = new MapSplitter(srcDivPair.getSource(), zoom);
 				MapArea[] areas = splitter.split(orderByDecreasingArea);
@@ -1356,8 +1359,8 @@ public class MapBuilder implements Configurable {
 
 				// preserve the end points of horizontal and vertical lines that lie
 				// on the bbox of the shape. 
-				if(last.getLatitude() == prev.getLatitude() && (last.getLatitude() == minLat || last.getLatitude() == maxLat) ||
-				   last.getLongitude() == prev.getLongitude() && (last.getLongitude() == minLon || last.getLongitude() == maxLon)) {
+				if(last.getHighPrecLat() == prev.getHighPrecLat() && (last.getLatitude() == minLat || last.getLatitude() == maxLat) ||
+				   last.getHighPrecLon() == prev.getHighPrecLon() && (last.getLongitude() == minLon || last.getLongitude() == maxLon)) {
 					last.preserved(true);
 					prev.preserved(true);
 				}
@@ -1547,4 +1550,91 @@ public class MapBuilder implements Configurable {
 			map.addMapObject(pg);
 		}
 	}
+
+	/**
+	 * Merge shapes and remove holes which are too small at the given zoom level.
+	 * May increase the minimum resolution of existing shapes and add new (merged)
+	 * for the given resolution. 
+	 * 
+	 * @param srcDivPair the map source
+	 * @param zoom       the zoom
+	 */
+	private void mergeShapesFirst(SourceSubdiv srcDivPair, Zoom zoom) {
+		final int res = zoom.getResolution();
+		final int minSize = getMinSizePolygonForResolution(res) * (1 << zoom.getShiftValue());
+		
+		MapDataSource mapSource = srcDivPair.getSource();
+		List<MapShape> shapesThisLevel = mapSource.getShapes().stream()
+				.filter(s -> s.getMinResolution() <= res && s.getMaxResolution()>= res)
+				.collect(Collectors.toList());
+		List<MapShape> copies = new ArrayList<>(shapesThisLevel.size());
+		for (MapShape s : shapesThisLevel) {
+			MapShape copy = s.copy();
+			copy.setPoints(s.getPoints());
+			s.setMinResolution(res+1);
+			copy.setMaxResolution(res);
+			copies.add(copy);
+		}
+		for (MapShape s : copies) {
+			removeTooSmallHoles(s.getPoints(), minSize);
+		}
+		ShapeMergeFilter shapeMergeFilter = new ShapeMergeFilter(res, orderByDecreasingArea);
+		List<MapShape> merged = shapeMergeFilter.merge(copies);
+		mapSource.getShapes().addAll(merged);
+		
+	}
+
+	public static void removeTooSmallHoles(List<Coord> points , int minSize) {
+		// possibly rotate shape so that start point is on the boundary -> not part of a hole 
+		int minLatHp = Integer.MAX_VALUE;
+		int rotate = -1;
+		for (int i = 0; i < points.size(); i++) {
+			int latHp = points.get(i).getHighPrecLat();
+			if (latHp < minLatHp) {
+				minLatHp = latHp;
+				rotate = i;
+			}
+		}
+		if (rotate > 0) {
+			points.remove(points.size() - 1);
+			Collections.rotate(points, -rotate);
+			points.add(points.get(0));
+		}
+		Area bbox = Area.getBBox(points);
+		
+		points.forEach(Coord::resetHighwayCount);
+		points.forEach(Coord::incHighwayCount);
+		
+		for (int i = 0; i < points.size(); i++) {
+			Coord p0 = points.get(i);
+			if (p0.getHighwayCount() <= 1)
+				continue;
+			if (i == points.size() - 1)
+				return;
+			for (int j = points.size() - 2; j > i; j--) {
+				if (p0 == points.get(j)) {
+					List<Coord> hole = points.subList(i, j + 1);
+					Area bboxHole = Area.getBBox(hole);
+					boolean outer = false;
+					if (bboxHole.equals(bbox)) {
+						// we travelled along the outer shape, the rest must be the hole
+						hole = points.subList(j, points.size());
+						bboxHole = Area.getBBox(hole);
+						outer = true;
+					}
+					if (bboxHole.getMaxDimension() < minSize) {
+						if (!outer)
+							points.subList(i, j).clear(); 
+						else
+							points = points.subList(i, j);
+						p0.decHighwayCount();
+						break;
+					} else {
+						log.diagnostic("hole too large");
+					}
+				}
+			}
+		}
+	}
+
 }

branches/low-res-opt/src/uk/me/parabola/mkgmap/filters/PolygonSplitterFilter.java
@@ -18,8 +18,10 @@ package uk.me.parabola.mkgmap.filters;
 
 import uk.me.parabola.imgfmt.app.Area;
 import uk.me.parabola.imgfmt.app.Coord;
+import uk.me.parabola.log.Logger;
 import uk.me.parabola.mkgmap.general.MapElement;
 import uk.me.parabola.mkgmap.general.MapShape;
+import uk.me.parabola.mkgmap.reader.osm.GType;
 import uk.me.parabola.util.ShapeSplitter;
 
 import java.util.ArrayList;
@@ -31,6 +33,8 @@ import java.util.List;
  * @author Gerd Petermann
  */
 public class PolygonSplitterFilter implements MapFilter {
+	private static final Logger log = Logger.getLogger(PolygonSplitterFilter.class);
+
 	public static final int MAX_POINT_IN_ELEMENT = 250;
 	private int shift;
 	
@@ -80,6 +84,9 @@ public class PolygonSplitterFilter implements MapFilter {
 		try {
 			next.doFilter(shape);
 		} catch (MustSplitException e) {
+			if (log.isDebugEnabled()) {
+				log.debug("splitting shape", GType.formatType(shape.getType()), "at shift", shift);
+			}
 			List<MapShape> outputs = new ArrayList<>();
 			split(shape, outputs); // split in half
 			for (MapShape s : outputs) {

branches/low-res-opt/src/uk/me/parabola/mkgmap/filters/ShapeMergeFilter.java
@@ -42,10 +42,22 @@ public class ShapeMergeFilter{
 	private final int resolution;
 	private static final ShapeHelper DUP_SHAPE = new ShapeHelper(new ArrayList<>(0)); 
 	private final boolean orderByDecreasingArea;
+	private final int maxPoints;
 
+	/**
+	 * Create the shape filter with the given attributes. It will ignore shapes
+	 * which are not displayed at the given resolution.
+	 * 
+	 * @param resolution            the resolution
+	 * @param orderByDecreasingArea if true, only certain shapes are merged
+	 */
 	public ShapeMergeFilter(int resolution, boolean orderByDecreasingArea) {
 		this.resolution = resolution;
 		this.orderByDecreasingArea = orderByDecreasingArea;
+		if (resolution < 24)
+			maxPoints = Integer.MAX_VALUE; // let ShapeSplitter split if too complex
+		else 
+			maxPoints = PolygonSplitterFilter.MAX_POINT_IN_ELEMENT;
 	}
 
 	/**
@@ -160,7 +172,7 @@ public class ShapeMergeFilter{
 					sharedPoints.add(c);
 				}
 			}
-			if (sharedPoints.isEmpty() || sh0.getPoints().size() - sharedPoints.size()> PolygonSplitterFilter.MAX_POINT_IN_ELEMENT) {
+			if (sharedPoints.isEmpty() || (sh0.getPoints().size() - sharedPoints.size() > maxPoints)) {
 				// merge will not work 
 				noMerge.add(sh0);
 				continue;
@@ -265,7 +277,7 @@ public class ShapeMergeFilter{
 			if (mergeRes == shOld){
 				result.add(shOld);
 				continue;
-			} else if (mergeRes != null){
+			} else {
 				shNew = mergeRes;
 			}
 			if (shNew == DUP_SHAPE){
@@ -273,9 +285,9 @@ public class ShapeMergeFilter{
 				return list; // nothing to do
 			}
 		}
-		if (shNew != null && shNew != DUP_SHAPE)
+		if (shNew != DUP_SHAPE)
 			result.add(shNew);
-		if (result.size() > list.size()+1 )
+		if (result.size() > list.size() + 1)
 			log.error("result list size is wrong", list.size(), "->", result.size());
 		return result;
 	}
@@ -287,7 +299,7 @@ public class ShapeMergeFilter{
 	 * @return merged shape or 1st shape if no common point found or {@code dupShape} 
 	 * if both shapes describe the same area. 
 	 */
-	private static ShapeHelper tryMerge(ShapeHelper sh1, ShapeHelper sh2) {
+	private ShapeHelper tryMerge(ShapeHelper sh1, ShapeHelper sh2) {
 		
 		// both clockwise or both ccw ?
 		boolean sameDir = sh1.areaTestVal > 0 && sh2.areaTestVal > 0 || sh1.areaTestVal < 0 && sh2.areaTestVal < 0;
@@ -317,40 +329,36 @@ public class ShapeMergeFilter{
 				return DUP_SHAPE;
 			}
 		}
-		List<Coord> merged = null; 
-		if (points1.size() + points2.size() - 2*sh1PositionsToCheck.size() < PolygonSplitterFilter.MAX_POINT_IN_ELEMENT){
+		List<Coord> merged = null;
+		if (points1.size() + points2.size() - 2 * sh1PositionsToCheck.size() < maxPoints) {
 			merged = mergeLongestSequence(points1, points2, sh1PositionsToCheck, sh2PositionsToCheck, sameDir);
+			if (merged == null)
+				return sh1;
 			if (merged.isEmpty())
 				return DUP_SHAPE;
-			if (merged.get(0) != merged.get(merged.size()-1))
+			if (merged.get(0) != merged.get(merged.size() - 1))
 				merged = null;
-			else if (merged.size() > PolygonSplitterFilter.MAX_POINT_IN_ELEMENT){
-				// don't merge because merged polygon would be split again
+			else if (merged.size() > maxPoints) {
+				// don't merge because merged polygon would probably be split again
 				log.info("merge rejected: merged shape has too many points " + merged.size());
 				merged = null;
 			}
 		}
-		ShapeHelper shm = null;
-		if (merged != null){
-			shm = new ShapeHelper(merged);
-			if (Math.abs(shm.areaTestVal) != Math.abs(sh1.areaTestVal) + Math.abs(sh2.areaTestVal)){
-				log.warn("merging shapes skipped for shapes near", points1.get(sh1PositionsToCheck.getInt(0)).toOSMURL(), 
-						"(maybe overlapping shapes?)");
-				merged = null;
-				shm = null;
-			} else {
-				if (log.isInfoEnabled()){
-					log.info("merge of shapes near",points1.get(sh1PositionsToCheck.getInt(0)).toOSMURL(), 
-							"reduces number of points from",(points1.size()+points2.size()),
-							"to",merged.size());
-				}
-			}
-		}
-		if (shm != null)
-			return shm;
 		if (merged == null)
 			return sh1;
-		return null;
+		ShapeHelper shm = new ShapeHelper(merged);
+		if (Math.abs(shm.areaTestVal) != Math.abs(sh1.areaTestVal) + Math.abs(sh2.areaTestVal)){
+			log.warn("merging shapes skipped for shapes near", points1.get(sh1PositionsToCheck.getInt(0)).toOSMURL(), 
+					"(maybe overlapping shapes?)");
+			return sh1;
+		} else {
+			if (log.isInfoEnabled()){
+				log.info("merge of shapes near",points1.get(sh1PositionsToCheck.getInt(0)).toOSMURL(), 
+						"reduces number of points from",(points1.size()+points2.size()),
+						"to",merged.size());
+			}
+		}
+		return shm;
 	}
 
 	/**

branches/low-res-opt/src/uk/me/parabola/mkgmap/reader/osm/MultiPolygonCutter.java
@@ -29,7 +29,6 @@ import it.unimi.dsi.fastutil.longs.Long2ObjectOpenHashMap;
 import uk.me.parabola.imgfmt.Utils;
 import uk.me.parabola.imgfmt.app.Coord;
 import uk.me.parabola.log.Logger;
-import uk.me.parabola.mkgmap.reader.osm.MultiPolygonRelation.JoinedWay;
 import uk.me.parabola.util.Java2DConverter;
 
 /**
@@ -44,15 +43,18 @@ public class MultiPolygonCutter {
 	private static final Logger log = Logger.getLogger(MultiPolygonCutter.class);
 	private final MultiPolygonRelation rel;
 	private final Area tileArea;
+	private final Long2ObjectOpenHashMap<Coord> commonCoordMap;
 
 	/**
 	 * Create cutter for a given MP-relation and tile
 	 * @param multiPolygonRelation the MP-relation
 	 * @param tileArea the java area of the tile
 	 */
-	public MultiPolygonCutter(MultiPolygonRelation multiPolygonRelation, Area tileArea) {
+	public MultiPolygonCutter(MultiPolygonRelation multiPolygonRelation, Area tileArea, Long2ObjectOpenHashMap<Coord> commonCoordMap) {
 		rel = multiPolygonRelation;
 		this.tileArea = tileArea;
+		this.commonCoordMap = commonCoordMap;
+
 	}
 
 	/**
@@ -69,7 +71,7 @@ public class MultiPolygonCutter {
 	 */
 	public List<Way> cutOutInnerPolygons(Way outerPolygon, List<Way> innerPolygons) {
 		if (innerPolygons.isEmpty()) {
-			Way outerWay = new JoinedWay(outerPolygon);
+			Way outerWay = new Way(outerPolygon.getId(), outerPolygon.getPoints());
 			if (log.isDebugEnabled()) {
 				log.debug("Way", outerPolygon.getId(), "splitted to way", outerWay.getId());
 			}
@@ -102,33 +104,14 @@ public class MultiPolygonCutter {
 			finishedAreas.addAll(outerAreas);
 		} else if (outerAreas.size() == 1) {
 			// there is one outer area only
-			// it is checked before that all inner areas are inside this outer area
+			// it is assumed that all inner areas are inside this outer area
 			AreaCutData initialCutData = new AreaCutData();
 			initialCutData.outerArea = outerAreas.get(0);
 			initialCutData.innerAreas = innerAreas;
 			areasToCut.add(initialCutData);
 		} else {
-			// multiple outer areas
-			for (Area outerArea : outerAreas) {
-				AreaCutData initialCutData = new AreaCutData();
-				initialCutData.outerArea = outerArea;
-				initialCutData.innerAreas = new ArrayList<>(innerAreas
-						.size());
-				for (Area innerArea : innerAreas) {
-					if (outerArea.getBounds2D().intersects(
-						innerArea.getBounds2D())) {
-						initialCutData.innerAreas.add(innerArea);
-					}
-				}
-				
-				if (initialCutData.innerAreas.isEmpty()) {
-					// this is either an error
-					// or the outer area has been cut into pieces on the tile bounds
-					finishedAreas.add(outerArea);
-				} else {
-					areasToCut.add(initialCutData);
-				}
-			}
+			// initial data contains multiple outer areas
+			combineOuterAndInner(outerAreas, innerAreas, finishedAreas, areasToCut);
 		}
 
 		while (!areasToCut.isEmpty()) {
@@ -147,7 +130,7 @@ public class MultiPolygonCutter {
 				areaCutData.outerArea.subtract(cutPoint.getAreas().get(0));
 			else {
 				// first combine the areas that should be subtracted
-				Path2D.Double path = new Path2D.Double();
+				Path2D.Double path = new Path2D.Double(Path2D.WIND_NON_ZERO, innerAreas.size() * 20);
 				for (Area cutArea : cutPoint.getAreas()) {
 					path.append(cutArea, false);
 				}
@@ -192,48 +175,13 @@ public class MultiPolygonCutter {
 				// Now find the intersection of these two boxes with the
 				// original polygon. This will make two new areas, and each
 				// area will be one (or more) polygons.
-				Area a1 = new Area(r1); 
-				Area a2 = new Area(r2);
-				a1.intersect(areaCutData.outerArea);
-				a2.intersect(areaCutData.outerArea);
-				if (areaCutData.innerAreas.isEmpty()) {
-					finishedAreas.addAll(Java2DConverter.areaToSingularAreas(a1));
-					finishedAreas.addAll(Java2DConverter.areaToSingularAreas(a2));
-				} else {
-					ArrayList<Area> cuttedAreas = new ArrayList<>();
-					cuttedAreas.addAll(Java2DConverter.areaToSingularAreas(a1));
-					cuttedAreas.addAll(Java2DConverter.areaToSingularAreas(a2));
-					
-					for (Area nextOuterArea : cuttedAreas) {
-						ArrayList<Area> nextInnerAreas = null;
-						// go through all remaining inner areas and check if they
-						// must be further processed with the nextOuterArea 
-						for (Area nonProcessedInner : areaCutData.innerAreas) {
-							if (nextOuterArea.intersects(nonProcessedInner.getBounds2D())) {
-								if (nextInnerAreas == null) {
-									nextInnerAreas = new ArrayList<>();
-								}
-								nextInnerAreas.add(nonProcessedInner);
-							}
-						}
-						
-						if (nextInnerAreas == null || nextInnerAreas.isEmpty()) {
-							finishedAreas.add(nextOuterArea);
-						} else {
-							AreaCutData outCutData = new AreaCutData();
-							outCutData.outerArea = nextOuterArea;
-							outCutData.innerAreas= nextInnerAreas;
-							areasToCut.add(outCutData);
-						}
-					}
-				}
+				cutWithRectangle(r1, areaCutData, finishedAreas, areasToCut);
+				cutWithRectangle(r2, areaCutData, finishedAreas, areasToCut);
 			}
-			
 		}
 		
 		// convert the java.awt.geom.Area back to the mkgmap way
 		List<Way> cuttedOuterPolygon = new ArrayList<>(finishedAreas.size());
-		Long2ObjectOpenHashMap<Coord> commonCoordMap = new Long2ObjectOpenHashMap<>();
 		for (Area area : finishedAreas) {
 			Way w = singularAreaToWay(area, rel.getOriginalId());
 			if (w != null) {
@@ -263,6 +211,66 @@ public class MultiPolygonCutter {
 		return cuttedOuterPolygon;
 	}
 	
+	/**
+	 * Intersect area with cut rectangle.
+	 * Find the intersection of the rectangle with the original polygon and decide if the result is added to the 
+	 * finished areas or if another cut is needed.
+	 * 
+	 * @param cutRect the cut rectangle
+	 * @param areaCutData the cut data
+	 * @param finishedAreas list of finished areas
+	 * @param areasToCut queue with unfinished cut data
+	 */
+	private static void cutWithRectangle(Rectangle2D cutRect, AreaCutData areaCutData, Collection<Area> finishedAreas,
+			Queue<AreaCutData> areasToCut) {
+		
+		Area outer = new Area(cutRect); 
+		outer.intersect(areaCutData.outerArea);
+		final List<Area> dividedAreas = Java2DConverter.areaToSingularAreas(outer);
+		if (areaCutData.innerAreas.isEmpty()) {
+			finishedAreas.addAll(dividedAreas);
+			return;
+		} 
+		combineOuterAndInner(dividedAreas, areaCutData.innerAreas, finishedAreas, areasToCut);
+	}
+
+	/**
+	 * Combine each outer area which all those inner area that are likely to
+	 * intersect the outer. If no inner is found the outer area is finished, else
+	 * the new combination is added to the queue.
+	 * 
+	 * @param outerAreas    list of outer areas
+	 * @param innerAreas    list of all inner areas
+	 * @param finishedAreas list of finished areas
+	 * @param areasToCut    queue with unfinished cut data
+	 */
+	private static void combineOuterAndInner(List<Area> outerAreas, List<Area> innerAreas,
+			Collection<Area> finishedAreas, Queue<AreaCutData> areasToCut) {
+		for (Area nextOuterArea : outerAreas) {
+			ArrayList<Area> nextInnerAreas = null;
+			// go through all remaining inner areas and check if they
+			// must be further processed with the nextOuterArea
+			
+			for (Area nonProcessedInner : innerAreas) {
+				if (nextOuterArea.intersects(nonProcessedInner.getBounds2D())) {
+					if (nextInnerAreas == null) {
+						nextInnerAreas = new ArrayList<>();
+					}
+					nextInnerAreas.add(nonProcessedInner);
+				}
+			}
+
+			if (nextInnerAreas == null) {
+				finishedAreas.add(nextOuterArea);
+			} else {
+				AreaCutData outCutData = new AreaCutData();
+				outCutData.outerArea = nextOuterArea;
+				outCutData.innerAreas = nextInnerAreas;
+				areasToCut.add(outCutData);
+			}
+		}
+	}
+
 	private static CutPoint calcNextCutPoint(AreaCutData areaData) {
 		if (areaData.innerAreas == null || areaData.innerAreas.isEmpty()) {
 			return null;
@@ -281,66 +289,26 @@ public class MultiPolygonCutter {
 			} else {
 				return cutPoint2;
 			}
-			
 		}
 		
-		ArrayList<Area> innerStart = new ArrayList<>(areaData.innerAreas);
+		ArrayList<Area> innersSorted = new ArrayList<>(areaData.innerAreas);
 		
-		// first try to cut out all polygons that intersect the boundaries of the outer polygon
-		// this has the advantage that the outer polygon need not be split into two halves
+		CutPoint bestCutPoint = null;
 		for (CoordinateAxis axis : CoordinateAxis.values()) {
-			CutPoint edgeCutPoint = new CutPoint(axis, outerBounds);
-
-			// go through the inner polygon list and use all polygons that intersect the outer polygons bbox at the start
-			innerStart.sort(axis == CoordinateAxis.LONGITUDE ? COMP_LONG_START: COMP_LAT_START);
-			for (Area anInnerStart : innerStart) {
-				if (axis.getStartHighPrec(anInnerStart) <= axis.getStartHighPrec(outerBounds)) {
-					// found a touching area
-					edgeCutPoint.addArea(anInnerStart);
-				} else {
-					break;
-				}
-			}
-			if (edgeCutPoint.getNumberOfAreas() > 0) {
-				// there at least one intersecting inner polygon
-				return edgeCutPoint;
-			}
-			
-			innerStart.sort(axis == CoordinateAxis.LONGITUDE ? COMP_LONG_STOP: COMP_LAT_STOP);
-			// go through the inner polygon list and use all polygons that intersect the outer polygons bbox at the stop
-			for (Area anInnerStart : innerStart) {
-				if (axis.getStopHighPrec(anInnerStart) >= axis.getStopHighPrec(outerBounds)) {
-					// found a touching area
-					edgeCutPoint.addArea(anInnerStart);
-				} else {
-					break;
-				}
-			}
-			if (edgeCutPoint.getNumberOfAreas() > 0) {
-				// there at least one intersecting inner polygon
-				return edgeCutPoint;
-			}
-		}
-		
-		
-		ArrayList<CutPoint> bestCutPoints = new ArrayList<>(CoordinateAxis.values().length);
-		for (CoordinateAxis axis : CoordinateAxis.values()) {
-			CutPoint bestCutPoint = new CutPoint(axis, outerBounds);
 			CutPoint currentCutPoint = new CutPoint(axis, outerBounds);
 
-			innerStart.sort(axis == CoordinateAxis.LONGITUDE ? COMP_LONG_START: COMP_LAT_START);
+			innersSorted.sort(axis == CoordinateAxis.LONGITUDE ? COMP_LONG_START: COMP_LAT_START);
 
-			for (Area anInnerStart : innerStart) {
-				currentCutPoint.addArea(anInnerStart);
+			for (Area inner : innersSorted) {
+				currentCutPoint.addArea(inner);
 
-				if (currentCutPoint.compareTo(bestCutPoint) > 0) {
+				if (bestCutPoint == null || currentCutPoint.compareTo(bestCutPoint) > 0) {
 					bestCutPoint = currentCutPoint.duplicate();
 				}
 			}
-			bestCutPoints.add(bestCutPoint);
 		}
 
-		return Collections.max(bestCutPoints);
+		return bestCutPoint;
 		
 	}
 
@@ -432,14 +400,6 @@ public class MultiPolygonCutter {
 			return Math.min(d1, d2) < CUT_POINT_CLASSIFICATION_BAD_THRESHOLD;
 		}
 		
-		private boolean isStartCut() {
-			return (startPoinHp <= axis.getStartHighPrec(outerBounds));
-		}
-		
-		private boolean isStopCut() {
-			return (stopPointHp >= axis.getStopHighPrec(outerBounds));
-		}
-		
 		/**
 		 * Calculates the point where the cut should be applied.
 		 * @return the point of cut
@@ -452,20 +412,6 @@ public class MultiPolygonCutter {
 			
 			if (startPoinHp == stopPointHp) {
 				// there is no choice => return the one possible point 
-				cutPointHp = startPoinHp;
-				return cutPointHp;
-			}
-			
-			if (isStartCut()) {
-				// the polygons can be cut out at the start of the sector
-				// thats good because the big polygon need not to be cut into two halves
-				cutPointHp = startPoinHp;
-				return cutPointHp;
-			}
-			
-			if (isStopCut()) {
-				// the polygons can be cut out at the end of the sector
-				// thats good because the big polygon need not to be cut into two halves
 				cutPointHp = startPoinHp;
 				return cutPointHp;
 			}
@@ -591,18 +537,10 @@ public class MultiPolygonCutter {
 			return minAspectRatio;
 		}
 		
-		
 		public int compareTo(CutPoint o) {
 			if (this == o) {
 				return 0;
 			}
-			// prefer a cut at the boundaries
-			int d = Boolean.compare(isStartCut(), o.isStartCut());
-			if (d != 0)
-				return d;
-			d = Boolean.compare(isStopCut(), o.isStopCut());
-			if (d != 0)
-				return d;
 			// handle the special case that a cut has no area
 			if (getNumberOfAreas() == 0) {
 				return o.getNumberOfAreas() == 0 ? 0 : -1;
@@ -610,7 +548,7 @@ public class MultiPolygonCutter {
 				return 1;
 			}
 
-			d = Boolean.compare(o.isBadCutPoint(), isBadCutPoint()); // exchanged order!
+			int d = Boolean.compare(o.isBadCutPoint(), isBadCutPoint()); // exchanged order!
 			if (d != 0)
 				return d;
 			double dAR = getMinAspectRatio() - o.getMinAspectRatio();

branches/low-res-opt/src/uk/me/parabola/mkgmap/reader/osm/MultiPolygonRelation.java
@@ -38,6 +38,7 @@ import java.util.concurrent.LinkedBlockingQueue;
 import java.util.logging.Level;
 import java.util.stream.Collectors;
 
+import it.unimi.dsi.fastutil.longs.Long2ObjectOpenHashMap;
 import uk.me.parabola.imgfmt.app.Coord;
 import uk.me.parabola.log.Logger;
 import uk.me.parabola.util.Java2DConverter;
@@ -72,6 +73,7 @@ public class MultiPolygonRelation extends Relation {
 	
 	protected double largestSize;
 	protected JoinedWay largestOuterPolygon;
+	private Long2ObjectOpenHashMap<Coord> commonCoordMap = new Long2ObjectOpenHashMap<>(); 
 	
 	protected Set<Way> outerWaysForLineTagging;
 	protected Map<String, String> outerTags;
@@ -972,7 +974,7 @@ public class MultiPolygonRelation extends Relation {
 						innerWays.add(polygonHoleStatus.polygon);
 					}
 
-					MultiPolygonCutter cutter = new MultiPolygonCutter(this, tileArea);
+					MultiPolygonCutter cutter = new MultiPolygonCutter(this, tileArea, commonCoordMap);
 					singularOuterPolygons = cutter.cutOutInnerPolygons(currentPolygon.polygon, innerWays);
 				}
 				

branches/low-res-opt/src/uk/me/parabola/util/IsInUtil.java
@@ -175,10 +175,15 @@ public class IsInUtil {
 			status |= ON;
 		if (status == ON) {
 			// found no intersection and first point is on boundary
-			if (onBoundary.cardinality() != n) {
+			while (onBoundary.cardinality() != n) {
+				int pos = onBoundary.nextClearBit(0);
 				// return result for first point which is not on boundary
-				Coord pTest = lineToTest.get(onBoundary.nextClearBit(0));
+				Coord pTest = lineToTest.get(pos);
 				status |= isPointInShape(pTest, shape);
+				if (status == ON) {
+					onBoundary.set(pos);
+					continue;
+				}
 				return status;
 			}
 			status |= checkAllOn(lineToTest, shape);

commit 4c2724557cefd5761cf9ac05b9bac33281704cd6

fix major bug introduced with r4729: small islands where removed from all levels, not only from overview map
TODO: some small islands are not removed in the first call of removeTooSmallHoles()

git-svn-id: https://svn.mkgmap.org.uk/mkgmap/mkgmap@4742 25d90789-57f7-4ee0-8453-03a3dfeeeb22

branches/low-res-opt/src/uk/me/parabola/mkgmap/build/MapBuilder.java
@@ -1564,19 +1564,16 @@ public class MapBuilder implements Configurable {
 		List<MapShape> copies = new ArrayList<>(shapesThisLevel.size());
 		for (MapShape s : shapesThisLevel) {
 			MapShape copy = s.copy();
-			copy.setPoints(s.getPoints());
+			copy.setPoints(new ArrayList<>(s.getPoints()));
 			s.setMinResolution(res+1);
 			copy.setMaxResolution(res);
+			removeTooSmallHoles(copy.getPoints(), minSize);
 			copies.add(copy);
-		}
-		for (MapShape s : copies) {
-			removeTooSmallHoles(s.getPoints(), minSize);
 		}
 		ShapeMergeFilter shapeMergeFilter = new ShapeMergeFilter(res, orderByDecreasingArea);
 		List<MapShape> merged = shapeMergeFilter.merge(copies);
 		mapSource.getShapes().addAll(merged);
-		
-		}
+	}
 
 	public static void removeTooSmallHoles(List<Coord> points , int minSize) {
 		// possibly rotate shape so that start point is on the boundary -> not part of a hole 